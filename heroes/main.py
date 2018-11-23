ca6df9466056549389e012ab2b074e1a4d706514
zhanghui <zhanghui@huwochuxing.com>
2018-11-23 17:13:40 +0800

    抢票动态操作

--- heroes/main.py
+++ heroes/main.py
@@ -1,7 +1,12 @@
 from heroes.db import mysql
 import time
-
+from heroes.chrome import Chrome
 # mysql = mysql.mysql()
 # mysql.version()
 
-print(time.strftime('%Y-%m-%d',time.localtime(time.time())))
+# print(time.strftime('%Y-%m-%d',time.localtime(time.time())))
+# s = """ a = %.2f""" % (12.22)
+# print(s)
+
+ch = Chrome()
+ch.run()
