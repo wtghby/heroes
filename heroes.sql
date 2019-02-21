5da635f5e4c6822c94a3b0d1909ae4917c40ca1f
zhanghui <zhanghui@huwochuxing.com>
2019-02-21 16:12:55 +0800

    股票主键改为uuid格式

--- heroes.sql
+++ heroes.sql
@@ -11,11 +11,19 @@
  Target Server Version : 50717
  File Encoding         : 65001
 
- Date: 16/11/2018 17:14:33
+ Date: 21/02/2019 16:11:19
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for hibernate_sequence
+-- ----------------------------
+DROP TABLE IF EXISTS `hibernate_sequence`;
+CREATE TABLE `hibernate_sequence`  (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Fixed;
 
 -- ----------------------------
 -- Table structure for tb_department
@@ -30,17 +38,19 @@ CREATE TABLE `tb_department`  (
   `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '外键_股票代码',
   `ddate` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '日期',
   `type` tinyint(4) NOT NULL COMMENT '0-买入，1-卖出',
+  `stock_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '股票外键',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `pk_code`(`code`) USING BTREE,
-  CONSTRAINT `pk_code` FOREIGN KEY (`code`) REFERENCES `tb_stock` (`code`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 461 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  INDEX `fk_stock_department`(`code`, `ddate`) USING BTREE,
+  INDEX `ffk_stock_department`(`stock_id`) USING BTREE,
+  CONSTRAINT `ffk_stock_department` FOREIGN KEY (`stock_id`) REFERENCES `tb_stock` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 11111 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for tb_stock
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_stock`;
 CREATE TABLE `tb_stock`  (
-  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '主键',
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
   `price` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '价格',
   `increase` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '涨幅',
@@ -52,7 +62,8 @@ CREATE TABLE `tb_stock`  (
   `code` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '股票代码',
   `ddate` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '日期',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `code`(`code`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 47 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  INDEX `code`(`code`) USING BTREE,
+  INDEX `code_2`(`code`, `ddate`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
